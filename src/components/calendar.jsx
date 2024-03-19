f15f0cea209451c0fd324a50e3a7edcea37d58b2
EllenSuni <ellen.suni@outlook.com>
2024-03-19 14:40:51 +0100

    calendar done

---
+++ src/components/calendar.jsx
@@ -0,0 +1,15 @@
+import Calendar from "react-calendar";
+
+
+function CalendarView() {
+
+
+  return (
+    <div>
+      <Calendar />
+    </div>
+
+  );
+}
+
+export default CalendarView;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,11 @@
+import CalendarView from "../components/calendar"
+
 function Dashboard() {
   return (
+    <>
     <h1>Dashboard</h1>
+    <CalendarView/>
+    </>
   )
 }
 
