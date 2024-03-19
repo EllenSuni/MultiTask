4541ec7210fef1522373bfd5352aacd1c40e2c13
EllenSuni <ellen.suni@outlook.com>
2024-03-19 10:42:10 +0100

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import './App.css'
+import Dashboard from './pages/Dashboard'
+import ProjectPage from './pages/ProjectPage'
 
-function App() {
+import {
+  createHashRouter,
+  Link,
+  Outlet,
+  RouterProvider
+} from 'react-router-dom'
 
+function Root() {
   return (
     <>
-      <h1>Lab 1.2</h1>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/project">Project</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </>
   )
+}
+
+function App() {
+  const router = createHashRouter([
+    {
+      children: [
+        {element: <Dashboard/>, path: "/"},
+        {element: <ProjectPage/>, path: "/project"}
+      ],
+
+      element: <Root/>
+    }
+  ])
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
